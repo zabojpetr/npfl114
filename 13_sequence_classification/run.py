7232fc931c4b357d95d60372378e2ce8beaceefd
Petr Záboj <zaboj.petr@gmail.com>
2018-04-19 09:21:46 +0200

    Sequence classification and sequence prediction

---
+++ 13_sequence_classification/run.py
@@ -0,0 +1,19 @@
+import argparse
+import subprocess
+import os
+
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', default="test.txt", type=str, help = 'File with settings')
+    args = parser.parse_args()
+
+    file = open(args.file, "r")
+
+    for line in file.readlines():
+        # subprocess.Popen('ulimit -t unlimited; nice -n 19 python3 mnist_competition.py ' + line, shell=True)
+        # subprocess.run('ulimit -t unlimited; nice -n 19 python3 mnist_competition.py ' + line, shell=True)
+        print("Started: " + line)
+        os.system('python sequence_classification.py ' + line)
+        print()
+        print()
